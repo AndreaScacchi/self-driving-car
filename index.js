a005835abeddf652ff60f5d11398aa3f4b5555e4
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-15 21:40:13 +0100

    New files added via tsc

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Car {
+    constructor(props) {
+        this.isRunning = props.isRunning;
+    }
+}
+const autonomousCar = new Car({ isRunning: true });
