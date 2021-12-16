fcd884cd7370e449392311872737f3840a91d60a
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-16 11:48:38 +0100

    index.js file modified via tsc

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const computer_vision_1 = require("./computer-vision");
 class Car {
     constructor(props) {
         this.isRunning = props.isRunning;
     }
+    respond(events) {
+        if (!this.isRunning) {
+            return console.log('The car is off!');
+        }
+    }
 }
 const autonomousCar = new Car({ isRunning: true });
+autonomousCar.respond((0, computer_vision_1.getObstacleEvents)());
