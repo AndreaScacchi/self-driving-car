06803391b16742407e643c30176928ce835113c3
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-16 12:40:14 +0100

    index.js file modified via tsc

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ class SteeringControl {
 class Car {
     constructor(props) {
         this.isRunning = props.isRunning;
+        this.steeringControl = props.steeringControl;
     }
     respond(events) {
         if (!this.isRunning) {
             return console.log('The car is off!');
         }
+        Object.keys(events).forEach(eventKey => {
+            if (!events[eventKey]) {
+                return;
+            }
+            if (eventKey === 'ObstacleLeft') {
+                this.steeringControl.turn('right');
+            }
+            if (eventKey === 'ObstacleRight') {
+                this.steeringControl.turn('left');
+            }
+        });
     }
 }
 const steering = new SteeringControl();
-const autonomousCar = new Car({ isRunning: true });
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
 autonomousCar.respond((0, computer_vision_1.getObstacleEvents)());
