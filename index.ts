6333ada09fac101134025906a43db810db12f26c
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-16 11:25:17 +0100

    respond method implemented

--- index.ts
+++ index.ts
@@ -21,7 +21,9 @@ class Car implements AutonomousCar {
     }
 
     respond(events: Events) {
-
+        if(!this.isRunning) {
+            return console.log('The car is off!');
+        }
     }
 }
 
